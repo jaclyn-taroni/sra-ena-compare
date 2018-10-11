5a31ba96f0da770d66a7f6a4b23fea449c0f7124
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2018-10-11 11:46:03 -0400

    Add paired-end experiment

--- quantify.sh
+++ quantify.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir quant && cd quant
+
+# first do the single-end experiment
 mkdir SRP036035 && cd SRP036035
 mkdir SRA && mkdir ENA
 
@@ -23,5 +25,33 @@ do
 	salmon --no-version-check quant -l A -i transcriptome_index/hsapiens_short \
 	  -r $fastq -p 16 -o $output_directory \
 	  --seqBias --dumpEq --writeUnmappedNames
+  done
+done
+
+now the paired-end experiment
+cd quant && mkdir SRP023539 && cd SRP023539
+mkdir SRA && mkdir ENA
+
+cd ../..
+
+declare -a dir_arr=("ENA" "SRA")
+declare -a arr=("SRR873426" "SRR873427" "SRR873428" "SRR873429" "SRR873430")
+
+for samp in "${arr[@]}"
+do
+  for dir in "${dir_arr[@]}"
+  do
+  	input_directory="data/SRP023539/${dir}"
+  	fastq_1="${input_directory}/${samp}_1.fastq"
+    fastq_2="${input_directory}/${samp}_2.fastq"
+  	output_directory="quant/SRP023539/${dir}/${samp}"
+  if [ $dir == "ENA" ]
+  	then
+  	  fastq_1="${fastq_1}.gz"
+      fastq_2="${fastq_2}.gz"
+	fi
+	salmon --no-version-check quant -l A -i transcriptome_index/hsapiens_long \
+	  -1 $fastq_1 -2 $fastq_2 -p 12 -o $output_directory \
+	  --seqBias --dumpEq --writeUnmappedNames --biasSpeedSamp 5
   done
 done
